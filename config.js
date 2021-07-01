2c9b5dae8ef888f7acbacc68bce4bcfdc31e66e5
Minh311 <86767723+Minh311@users.noreply.github.com>
2021-07-01 16:40:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Em yêu anh có phải không nào 😙',
-    desc: ' Trả lời anh thật lòng nhé !',
+    desc: ' Thoát ra là đồng ý làm vợ anh r nhé !',
     btnYes: 'Yêu ơi là yêuuu <33',
     btnNo: 'Anh mơ à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
