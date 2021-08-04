76bc7fda829c2c24f7011fcf48123ce3cb5df43e
me <86767723+Minh311@users.noreply.github.com>
2021-08-04 18:36:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Thanh Vân à !',
+    introTitle: 'Anh Cương à !',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Em yêu anh có phải không nào 😙',
-    desc: ' Thoát ra là đồng ý làm vợ anh r nhé !',
+    title: 'Anh yêu em có phải không nào 😙',
+    desc: ' Thoát ra là đồng ý làm chồng em r nhé !',
     btnYes: 'Yêu ơi là yêuuu <33',
-    btnNo: 'Anh mơ à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnNo: 'Em mơ à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Thế hẹn hết dịch anh qua đón nhé công chúa.',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Thế hẹn hết dịch em qua đón nhé anh iu.',
     btnAccept: 'Okiii lun <3',
 }
